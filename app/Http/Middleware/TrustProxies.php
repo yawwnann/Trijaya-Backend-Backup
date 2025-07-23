6d0076738200f5dcc75cd8d745d0569da51400d4
yawwnann <c0478592@gmail.com>
2025-07-23 20:38:20 +0700

    Enhance CORS settings to support new origins for improved API access

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*';
+}
